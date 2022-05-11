138da5b664c047d654ddd1bc975f2dc45eace1de
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-11 15:14:50 +0100

    Requesting method added

--- starwars/starships.py
+++ starwars/starships.py
@@ -1,10 +1,21 @@
 import starwars.app.requesting_sw as rq
+import json
 
 class Starships():
     def __init__(self):
         self.content = {}
+        self.starships = {}
 
 
-        def request(self):
-            pass
+    def requesting(self):
+        try:
+            self.content = json.loads(rq.sw.content)
+            print("Code 200: request successful")
+        except:
+            raise
+        finally:
+            print("Requests Executed")
+
+        return self.content
+
 
