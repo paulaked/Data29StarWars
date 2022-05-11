a7876e511b789f2ec298e67e7f59fd33c5e5aa30
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-11 16:59:23 +0100

    Get url added, all tests pass currently

--- starwars/tests/test_starship.py
+++ starwars/tests/test_starship.py
@@ -9,9 +9,22 @@ class MyTestCase(unittest.TestCase):
         self.starships = Starships()
 
     # Tests if requests gives a dictionary as a response
-    def test_request(self) -> None:
+    def test_request(self):
         actual = self.starships.requesting()
         self.assertEqual(type(actual), dict, "Type Check Failed: Dictionary expected")
+        self.assertIsNotNone(actual, "Dictionary is empty")
+
+    # Tests if requests gives a dictionary as a response
+    def test_get_starships(self):
+        self.starships.requesting()
+        actual = self.starships.get_starships()
+        self.assertEqual(type(actual), list, "Type Check Failed: List expected")
+        self.assertIsNotNone(actual, "List is empty")
+
+    # Tests if get_url gives a dictionary as a response
+    def test_get_url(self):
+        self.assertEqual(type(self.starships.get_url('https://www.swapi.tech/api/people/13')), dict,
+                         "Type Check Failed: Dictionary expected")
 
 
 if __name__ == '__main__':
