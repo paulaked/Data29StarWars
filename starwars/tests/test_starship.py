020dfd97cc22270ff564f40b554fedb3151fab7a
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-11 18:42:24 +0100

    tests for pilots being empty/ existing added

--- starwars/tests/test_starship.py
+++ starwars/tests/test_starship.py
@@ -26,6 +26,33 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(type(self.starships.get_url('https://www.swapi.tech/api/people/13')), dict,
                          "Type Check Failed: Dictionary expected")
 
+    # Tests if empty pilots returns none
+    def test_empty_pilots(self):
+        ship = {'model': 'CR90 corvette', 'starship_class': 'corvette',
+                'manufacturer': 'Corellian Engineering Corporation',
+                'cost_in_credits': '3500000',
+                'length': '150', 'crew': '30-165',
+                'passengers': '600', 'max_atmosphering_speed': '950',
+                'hyperdrive_rating': '2.0', 'MGLT': '60', 'cargo_capacity': '3000000',
+                'consumables': '1 year', 'pilots': [],
+                'created': '2020-09-17T17:55:06.604Z', 'edited': '2020-09-17T17:55:06.604Z',
+                'name': 'CR90 corvette', 'url': 'https://www.swapi.tech/api/starships/2'}
+        self.starships.empty_pilots(ship)
+        self.assertIs(ship["pilots"],None , "Value must be None")
+
+    # Tests if pilots exists returns not an empty list
+    def test_pilots_exist(self):
+        ship = {'model': 'YT-1300 light freighter', 'starship_class': 'Light freighter',
+                'manufacturer': 'Corellian Engineering Corporation', 'cost_in_credits': '100000',
+                'length': '34.37', 'crew': '4', 'passengers': '6', 'max_atmosphering_speed': '1050',
+                'hyperdrive_rating': '0.5', 'MGLT': '75', 'cargo_capacity': '100000',
+                'consumables': '2 months', 'pilots': ['https://www.swapi.tech/api/people/13', 'https://www.swapi.tech/api/people/14', 'https://www.swapi.tech/api/people/25', 'https://www.swapi.tech/api/people/31'],
+                'created': '2020-09-17T17:55:06.604Z', 'edited': '2020-09-17T17:55:06.604Z',
+                'name': 'Millennium Falcon', 'url': 'https://www.swapi.tech/api/starships/10'}
+        self.starships.pilots_exist(ship)
+        self.assertIsNotNone (ship["pilots"], "Values cannot be none")
+        self.assertIs(type(ship["pilots"]),list, "Must be a list")
+
 
 if __name__ == '__main__':
     unittest.main()
