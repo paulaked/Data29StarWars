ae5d699d58fdec9e8061826eeb850adfff2bf467
Durgesh Murugan <durgeshmurugan7962@gmail.com>
2022-05-11 16:01:54 +0100

    get_starship_api

--- starwars/app/starships.py
+++ starwars/app/starships.py
@@ -3,14 +3,11 @@ import pymongo
 import json
 import requests
 
+client = pymongo.MongoClient()
+db = client["starwars"]
+
+#db.drop_collection("starships")
+
 starships_req = requests.get('https://www.swapi.tech/api/starships%27')
-
-y = json.dumps(starships_req)
-
-for i in y:
-    print(i)
-
-
-
 
 
