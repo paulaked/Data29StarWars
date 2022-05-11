43cead11f722fb8080dfb4fd13c753b970394311
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-11 19:00:45 +0100

    Description for each method added

--- starwars/starships.py
+++ starwars/starships.py
@@ -24,6 +24,7 @@ class Starships:
 
         return self.content
 
+    # Method to retrieve starship info
     def get_starships(self):
         try:
             self.starships = self.content["results"]
@@ -34,28 +35,34 @@ class Starships:
             print("Starships method executed")
         return self.starships
 
+    # Method to retrieve any content from a given URL in json
     def get_url(self,url):
         return json.loads(requests.get(url).content)
 
-
+    # Method to retrieve information for each ship from their URLS
     def get_ship_info(self):
         ships = []
         for ship in self.starships:
             ships.append(self.get_url(ship["url"]))
         for ship in ships:
             self.ship_info.append(ship["result"]["properties"])
+        return self.ship_info
 
+    # Sets pilot names to None
     def empty_pilots(self,ship):
         ship["pilots"] = None
         return ship
 
+    # Sets pilot names to names given in each URL
     def pilots_exist(self,ship):
         pilots = []
         for url in ship["pilots"]:
             pilots.append(self.get_url(url)["result"]["properties"]["name"])
         ship.update({"pilots": pilots})
+        print("Starship " + ship["name"] + " has pilots: " + ship["pilots"])
         return (ship)
 
+    # Method to set pilot names accordingly
     def get_pilot_info(self):
         for ship in self.ship_info:
             if ship["pilots"] == []:
