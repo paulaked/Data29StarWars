a3f1d3195fb10fabcedc66c7211239b7fda987a0
june <juneli@spartaglobal.com>
2022-05-11 19:38:33 +0100

    retrieve starship names and pilot urls

--- starwars/app/user_story_1.py
+++ starwars/app/user_story_1.py
@@ -1,9 +1,26 @@
 import requests
 
 
-def get_starship_data():
-    return requests.get("https://swapi.tech/api/starships")
+def get_starship_data() -> list:
+    return requests.get("https://swapi.tech/api/starships").json()["results"]
 
+starships_list = get_starship_data()
 
-starship_data = get_starship_data().json()["results"]
-print(starship_data)
+def get_starships_urls(list) -> list:
+    url_list = []
+    for starship in list:
+        url_list.append(starship["url"])
+    return url_list
+
+url_list = get_starships_urls(starships_list)
+
+def get_pilot_data(list) -> list:
+    pilot_data = []
+    for starship in list:
+        pilot_data.append({"ship_name": requests.get(starship).json()["result"]["properties"]["model"],
+                           "pilot_urls": requests.get(starship).json()["result"]["properties"]["pilots"]
+                        })
+    return pilot_data
+
+pilot_data = get_pilot_data(url_list)
+print(pilot_data)
--- starwars/tests/test_module_us1.py
+++ starwars/tests/test_module_us1.py
@@ -1,4 +1,13 @@
 import unittest
+from starwars.app import user_story_1
 
-def test_get_starship_data():
-    assert get_starship_data == 200
+class UnitTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data = user_story_1.get_starship_data()
+
+    def test_response_code(self):
+        assert self.data.status_code == 200
+
+    def test_type(self):
+        assert self.data.type == "class"
+
