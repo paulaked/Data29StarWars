a1fa7028418c4b51117945981c06a6a5e0a4fbb9
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-11 17:03:33 +0100

    get_starships, get_url, get_ship, get_pilot_info method written: need to test get_ship + get_pilot_info

--- starwars/starships.py
+++ starwars/starships.py
@@ -1,5 +1,6 @@
 import starwars.app.requesting_sw as rq
 import json
+import requests
 
 
 class Starships:
@@ -8,6 +9,7 @@ class Starships:
     def __init__(self):
         self.content = {}
         self.starships = {}
+        self.ship_info = []
 
     # Requesting data from api using request import
     # Converts bjson to json, stores as dictionary
@@ -21,3 +23,37 @@ class Starships:
             print("Requests Executed")
 
         return self.content
+
+    def get_starships(self):
+        try:
+            self.starships = self.content["results"]
+            print("Starships Retrieved")
+        except:
+            raise
+        finally:
+            print("Starships method executed")
+        return self.starships
+
+    def get_url(self,url):
+        return json.loads(requests.get(url).content)
+
+    def get_ship_info(self):
+        ships = []
+        for ship in self.starships:
+            ships.append(self.get_url(ship["url"]))
+        for ship in ships:
+            self.ship_info.append(ship["result"]["properties"])
+
+    def get_pilot_info(self):
+        for ship in self.ship_info:
+            if ship["pilots"] == []:
+                self.ship_info["pilots"] = None
+            else:
+                pilots =[]
+                for url in ship["pilots"]:
+                    pilots.append(self.get_url(url)["result"]["properties"]["name"])
+
+
+
+
+
