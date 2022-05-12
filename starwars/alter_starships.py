0f1af12dc40d24d4c651b117c9c57b7ed82134f5
shibasparta <sdeb@spartaglobal.com>
2022-05-12 10:39:55 +0100

    function created for getting api db and is working as intended. unittesting yet to be created

--- starwars/alter_starships.py
+++ starwars/alter_starships.py
@@ -1,4 +1,16 @@
 ## EXTRACT STARSHIPS DATASET
+import requests
+
+
+def get_api_online(link):
+    return requests.get(link)
+
+starships_req = get_api_online('https://www.swapi.tech/api/starships')
+characters_req = get_api_online('https://www.swapi.tech/api/people')
+
+
+
+
 ## EXTRACT PEOPLE DATASET
 ## TARGET PILOT KEYS VIA API VALUES
 ## REPLACE PILOT KEYS VALUES WITH VALUES FROM CHARACTERS/PEOPLE DATASET
