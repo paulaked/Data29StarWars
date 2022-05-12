d7a384f05fbe9a43cc44bd46df63c3dd9eb7aa29
ali mateen <ali-mateen98@hotmail.co.uk>
2022-05-12 15:24:38 +0100

    created a class and imported the relevant files and packages

--- starships.py
+++ starships.py
@@ -1,7 +1,17 @@
 import pymongo
+import requests
+import json
+import starwars.app.requesting_sw as r_q
 
 client = pymongo.MongoClient()
 db = client["starwars"]
 
 db.drop_collection("starships")
+
+
+class Starships:
+    def __init__(self):
+        self.ship_info = []
+        self.content = {}
+        self.star_ship = {}
 
