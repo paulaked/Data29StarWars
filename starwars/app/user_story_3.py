a500ad3f1c248b47ff6f7e90f29bd0775d89a48a
june <juneli@spartaglobal.com>
2022-05-12 11:36:07 +0100

    add psuedo code draft for user story 3

---
+++ starwars/app/user_story_3.py
@@ -0,0 +1,6 @@
+# create starships collection and insert starships to collection
+# - input is a list of dictionaries
+# - output will be a collection
+
+def create_starships_pilots_colleciton():
+    pass
