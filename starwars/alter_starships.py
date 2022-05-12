edd82b71a651248a2525028f55a70fded1974c0c
shibasparta <sdeb@spartaglobal.com>
2022-05-12 14:37:07 +0100

    now have access to 'pilot' key

--- starwars/alter_starships.py
+++ starwars/alter_starships.py
@@ -6,7 +6,16 @@ def request_api_online(link):
     return requests.get(link)
 
 starships_req = request_api_online('https://www.swapi.tech/api/starships')
+ss_json = starships_req.json()
 
 
 ## TARGET PILOT KEYS VIA API VALUES
+def change_pilot_values():
+    starships_pilot = ''
+    for i in ss_json['results']:
+        for pilot in request_api_online(i['url']).json()['result']['properties']['pilots']:
+            print(request_api_online(pilot).json()['result']['properties']['name'])
+
+change_pilot_values()
+
 ## REPLACE PILOT KEYS VALUES WITH VALUES FROM CHARACTERS/PEOPLE DATASET
