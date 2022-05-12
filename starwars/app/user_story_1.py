cd98c21e03c5d9e76a260ab70d9bbbc02954a36e
june <juneli@spartaglobal.com>
2022-05-12 09:24:30 +0100

    revert url changes

--- starwars/app/user_story_1.py
+++ starwars/app/user_story_1.py
@@ -1,4 +1,5 @@
 import requests
+import starwars.config_manager as conf
 
 
 def get_starship_data() -> list:
@@ -6,21 +7,21 @@ def get_starship_data() -> list:
 
 starships_list = get_starship_data()
 
-def get_starships_urls(list) -> list:
-    url_list = []
-    for starship in list:
-        url_list.append(starship["url"])
-    return url_list
-
-url_list = get_starships_urls(starships_list)
-
-def get_pilot_data(list) -> list:
-    pilot_data = []
-    for starship in list:
-        pilot_data.append({"ship_name": requests.get(starship).json()["result"]["properties"]["model"],
-                           "pilot_urls": requests.get(starship).json()["result"]["properties"]["pilots"]
-                        })
-    return pilot_data
-
-pilot_data = get_pilot_data(url_list)
-print(pilot_data)
+# def get_starships_urls(list) -> list:
+#     url_list = []
+#     for starship in list:
+#         url_list.append(starship["url"])
+#     return url_list
+#
+# url_list = get_starships_urls(starships_list)
+#
+# def get_pilot_data(list) -> list:
+#     pilot_data = []
+#     for starship in list:
+#         pilot_data.append({"ship_name": requests.get(starship).json()["result"]["properties"]["model"],
+#                            "pilot_urls": requests.get(starship).json()["result"]["properties"]["pilots"]
+#                         })
+#     return pilot_data
+#
+# pilot_data = get_pilot_data(url_list)
+# print(pilot_data)
