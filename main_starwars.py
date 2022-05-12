33052372dadd1c8b6562475e47ac75a7aa19a4ff
cex <cexhw@Louises-MBP.cust.communityfibre.co.uk>
2022-05-12 14:41:14 +0100

    Created For loop to extract all data from API

--- main_starwars.py
+++ main_starwars.py
@@ -1,7 +1,18 @@
 import json
 import requests
-r = requests.get('https://www.swapi.tech/api/starships')
-packages_json = r.json()
 
-packages_str = json.dumps(packages_json, indent=2)
-print(packages_str)
+# FUNCTION TO PULL ALL AVAILIBLE STARSHIPS FROM API
+starship_request = requests.get('https://www.swapi.tech/api/starships')
+starship_json = starship_request.json()
+characters_request = requests.get('https://www.swapi.tech/api/people')
+characters_json = characters_request.json() #requesting a json object of the result
+
+starship_str = json.dumps(starship_json) #converting the a json file
+characters_str = json.dumps(characters_json)
+print(starship_str)
+print(characters_str) #extracted all characters from the data to have also the ID's
+
+#FUNCTION TO CHECK INTO THE URL AND EXTRACT ALL STARSHIPS TO EXTRACT ALL AVAILIBLE DATA
+for i in starship_json['results']:
+    print(requests.get(i["url"]).json()) 
+
