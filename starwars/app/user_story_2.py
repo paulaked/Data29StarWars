b7c5c98d41c88bc152a0844a6b702cd908f01949
june <juneli@spartaglobal.com>
2022-05-12 11:30:56 +0100

    add psuedo code draft for user story 2

--- starwars/app/user_story_2.py
+++ starwars/app/user_story_2.py
@@ -1,5 +1,33 @@
-# query collection for pilot urls
-# make link between pilot urls and characters
-# - link by url.response.name - characters.name
+# i need to replace pilot urls with character object ids
+# i need a link between pilot urls and character object ids
+# - i can query pilot url for character name and link with character name in document
 
-# find character object id for pilot.url.name
+# parent - for each starship in the collection
+# overarching def, will hold child def(s) inside
+# - (child) for each starship
+# - get pilot url
+# - (child) for each pilot url
+# - query the url for person
+# - (child) for each person name
+# - match with name on characters collection
+# - (child) replace url with character object id
+
+
+# child - get pilot list, (pilot list might be empty)
+# - input is document from the starships collection
+# - output will be the pilot url list (if exists)
+
+# child - perform query on pilot url
+# - input will be pilot url
+# - output will be the pilot name (people name)
+
+# child - find match of pilot name from characters collection
+# - do a find on the characters collection based on the pilot name
+# - return object id for character
+
+# child - update the pilot field in starships collection
+
+# - iterate through pilot list
+# -- each item in pilot list is a url
+# -- so i will need to get a response from each url
+# -- if the
