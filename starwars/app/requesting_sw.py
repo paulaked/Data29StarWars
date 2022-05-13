986570bb1b8825c304407cd8990e10173b88ebe5
Tatenda Mukutirwa <tatendanelson1@gmail.com>
2022-05-13 14:06:48 +0100

    Managed to change the urls with pilot names

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -2,23 +2,60 @@ import starwars.config_manager as conf
 import requests
 import json
 
+# sw = requests.get("https://www.swapi.tech/api/starships")
+#
+# sw_1 = sw.json()
+# starships_1 = []
+# for i in sw_1['results']:
+#     x = requests.get(i['url']).json()
+#     pilot = x['result']['properties']
+#     starships_1.append(pilot)
+# print(starships_1)
+#
+#
+# for i in starships_1:
+#     i['pilot'] = 'ships'
+# print(starships_1)
+#
+#
+#
+#
+# for i in sw_1['results']:
+#     x = requests.get(i['url']).json()
+#     pilot = x['result']['properties']['pilots']
+#     if len(pilot) >= 1:
+#         new_pilot = pilot
+#         for n in new_pilot:
+#             w = requests.get(n).json()
+#             update_pilot = w['result']['properties']['name']
 sw = requests.get("https://www.swapi.tech/api/starships")
 
 sw_1 = sw.json()
-
+starships_1 = []
 for i in sw_1['results']:
     x = requests.get(i['url']).json()
-    pilot = x['result']
-    print(pilot)
+    pilot = x['result']['properties']
+    starships_1.append(pilot)
+print(starships_1)
 
-for i in sw_1['results']:
+
+
+for i in starships_1:
     x = requests.get(i['url']).json()
     pilot = x['result']['properties']['pilots']
     if len(pilot) >= 1:
         new_pilot = pilot
+        pilots = []
         for n in new_pilot:
+
             w = requests.get(n).json()
             update_pilot = w['result']['properties']['name']
-            print(update_pilot)
+            pilots.append(update_pilot)
+        i["pilots"] = pilots
+
+print(starships_1)
+
+
+
 
 
