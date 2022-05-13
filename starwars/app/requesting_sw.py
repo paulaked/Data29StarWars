ae7ee2d3b0c550e808b7f17e6d1856f1e68ea747
Tatenda Mukutirwa <tatendanelson1@gmail.com>
2022-05-13 17:56:45 +0100

    final product

--- starwars/app/requesting_sw.py
+++ starwars/app/requesting_sw.py
@@ -1,61 +1,52 @@
+import pymongo
 import starwars.config_manager as conf
 import requests
 import json
 
-# sw = requests.get("https://www.swapi.tech/api/starships")
-#
-# sw_1 = sw.json()
-# starships_1 = []
-# for i in sw_1['results']:
-#     x = requests.get(i['url']).json()
-#     pilot = x['result']['properties']
-#     starships_1.append(pilot)
-# print(starships_1)
-#
-#
-# for i in starships_1:
-#     i['pilot'] = 'ships'
-# print(starships_1)
-#
-#
-#
-#
-# for i in sw_1['results']:
-#     x = requests.get(i['url']).json()
-#     pilot = x['result']['properties']['pilots']
-#     if len(pilot) >= 1:
-#         new_pilot = pilot
-#         for n in new_pilot:
-#             w = requests.get(n).json()
-#             update_pilot = w['result']['properties']['name']
-sw = requests.get("https://www.swapi.tech/api/starships")
+client = pymongo.MongoClient()
+db = client["starwars"]
 
+sw = requests.get("https://www.swapi.tech/api/starships") # imports data from the first page of the api
 sw_1 = sw.json()
+
+
+def get_starships(sw_1): # gets the raw data
+    url = sw_1['next'] # filters the data so that the next page can be extracted
+    while url != None:
+        print(url)
+        content = requests.get(url).json() # gets the data from all four pages.
+        starship = content['results']
+        sw_1['results'].extend(starship) # this merges all the data pages into one
+        url = content['next']
+    return sw_1
+
+
+print(get_starships(sw_1))
+
+
 starships_1 = []
-for i in sw_1['results']:
+for i in sw_1['results']: # filters the data to display on the results key
     x = requests.get(i['url']).json()
-    pilot = x['result']['properties']
+    pilot = x['result']['properties'] # filters the data to display on the results key
     starships_1.append(pilot)
 print(starships_1)
 
 
-
 for i in starships_1:
     x = requests.get(i['url']).json()
-    pilot = x['result']['properties']['pilots']
-    if len(pilot) >= 1:
+    pilot = x['result']['properties']['pilots']  # filters the data to get pilot information
+    if len(pilot) >= 1:  # filters out all the empty pilot fields
         new_pilot = pilot
         pilots = []
         for n in new_pilot:
 
-            w = requests.get(n).json()
-            update_pilot = w['result']['properties']['name']
-            pilots.append(update_pilot)
+            w = requests.get(n).json() # pulls out 
+            update_pilot = w['result']['properties']['name']  # filters the names of the pilots
+            pilots.append(db.characters.find_one({"name": update_pilot}, {"_id": 1}))  # appends the pilots names
+            # with a list of ObjectIDs
         i["pilots"] = pilots
 
 print(starships_1)
 
-
-
-
-
+db.create_collection("starships")
+db.starships.insert_one(starships_1)
