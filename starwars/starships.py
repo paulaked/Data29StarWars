0ca28ca84029befae0cb2687b03e5eeb2e3212cf
fahimul <fahimul.h.96@gmail.com>
2022-05-13 15:03:54 +0100

    completed and run through tests

--- starwars/starships.py
+++ starwars/starships.py
@@ -1,18 +1,14 @@
-import starwars.app.requesting_sw as rq
-import json
-import requests
-import pymongo
-
-client = pymongo.MongoClient()
-db = client["starwars"]
-
-#db.drop_collection("starships")
+from starwars.app.requesting_sw import *
 
 
-class Starships:
+def test_the_source():
+    assert extract_data(web_address) == dict(extract_data(web_address))
 
-    # Class attributes
-    def __init__(self):
-        self.ship_info = []
-        self.content = {}
-        self.starships = {}
+
+def test_the_page_urls():
+    assert get_url_for_all_pages() == list(get_url_for_all_pages())
+
+
+def test_the_pilots_id():
+    assert get_and_replace_pilots_id() == list(get_and_replace_pilots_id())
+
