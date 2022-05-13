591e50efaf5023e18bc1bbb31fde9e5ca7b39c60
Durgesh Murugan <durgeshmurugan7962@gmail.com>
2022-05-13 14:04:19 +0100

    replace_pilots_with_ids

--- starwars/app/starships.py
+++ starwars/app/starships.py
@@ -1,80 +1,68 @@
-import requesting_sw
-import pymongo
-import json
+#import libraries
 import requests
+import pymongo
+
 
 client = pymongo.MongoClient()
-db = client["starwars"]
-
-#db.drop_collection("starships")
-
-starships_req = requests.get("https://www.swapi.tech/api/starships")
-characters_req = requests.get("https://www.swapi.tech/api/people")
+db = client['starwars']
 
 
-starships_req_json = starships_req.json()
+starships_address = 'https://www.swapi.tech/api/starships'
 
-characters_req_json = characters_req.json()
+def extract_data(url):
+    starship_url = requests.get(url)
+    starship_url = starship_url.json()
+    return starship_url
 
-
-#print(starships_req_json["results"])
-
-#for i in starships_req_json['results']:
-#    print(i['url'])
-
-#def get_url():
-#starship_urls = []
-
-#for i in starships_req_json['results']:
-#    print(requests.get(i['url']).json())
-    #starships_urls.append(requests.get(i['url']).json())
-
-urls_list = []
-
-#def get_urls():
-#    for i in starships_req_json["results"]:
-#        urls = requests.get(i['url']).json()
-#        print(urls["result"]['properties']['pilots'])
-
-#def get_people():
-#    for i in characters_req_json[]
-#    pass
-#{'message': 'ok', 'result': {'properties': {'model': 'CR90 corvette', 'starship_class': 'corvette', 'manufacturer': 'Corellian Engineering Corporation', 'cost_in_credits': '3500000', 'length': '150', 'crew': '30-165', 'passengers': '600', 'max_atmosphering_speed': '950', 'hyperdrive_rating': '2.0', 'MGLT': '60', 'cargo_capacity': '3000000', 'consumables': '1 year', 'pilots': [], 'created': '2020-09-17T17:55:06.604Z', 'edited': '2020-09-17T17:55:06.604Z', 'name': 'CR90 corvette', 'url': 'https://www.swapi.tech/api/starships/2'}, 'description': 'A Starship', '_id': '5f63a34fee9fd7000499be1e', 'uid': '2', '__v': 0}}
+starship_url_pages = [value['url'] for value in extract_data(starships_address)['results']]
+#print(starship_url_pages)
 
 
+def get_url_for_all_pages():
+    current_page = extract_data(starships_address)
+    while current_page['next'] != None:
+        current_page = requests.get(current_page['next'])
+        current_page = current_page.json()
+        for item in current_page['results']:
+            starship_url_pages.append(item['url'])
+    return starship_url_pages
 
-#for i in urls_list:
- #   properties = requests.get(i["result"]).json()
-  #  print(properties)
+def get_and_replace_pilots_id():
+    starships = []
+    for i in get_url_for_all_pages():
+        url_content = requests.get(i)
+        url_content = url_content.json()
+        result = url_content['result']
+        properties = result['properties']
+        pilot_url = properties['pilots']
+        pilot_name = []
+        for pilot in pilot_url:
+            pilot_content = requests.get(pilot)
+            pilot_content = pilot_content.json()
+            pilot_name.append(pilot_content['result']['properties']['name'])
+        pilots_id = []
+        for i in pilot_name:
+            for n in db.characters.find({'name': i}):
+                pilots_id.append(n["_id"])
+        url_content['result']['properties']['pilots'] = pilots_id
+        starships.append(url_content)
+
+    return starships
+
+# 7. Drop the existing starship collection from Mongodb:
+db.drop_collection("Starship")
+print('dropped')
+
+# 8. Creating the starship collecton if it doesn't exist on Mongodb:
+db.create_collection("Starship")
+print('the collection was created successfully')
+
+# 9. Different starships are added to the Starship collection on Mongodb:
+for i in get_and_replace_pilots_id():
+    db.Starship.insert_one(i)
 
 
-#print(starships_urls)
-
-#for i in starships_req_json['results']['properties']:
-#    print(requests.get(i['pilots']).json())
-
-#for i in characters_req_json['results']:
- #   print(requests.get(i['url']).json())
-
-for i in characters_req_json["results"]:
-    urls = requests.get(i['url']).json()
-    print(urls["result"]['_id'])
-
-#id call darth vader
+print("all the available starship are added successfully")
 
 
-# not urls, object ids of database
-
-#temp_ships = []
-#temp_pilots = []
-#x = starships_req_json["result"]["properties"]["name"]
-#pilots = starships_req_json["result"]["properties"]["pilots"]
-#if pilots != 0:
-#    for i in range(0,len(pilot)):
-#        temp_ships.append(x)
-#        temp_pilots.append(pilots[i])
-#else:
-#    print(x + "no pilot")
-
-#get_urls()
 
