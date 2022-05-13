d4fdbecde63c825cf6bd4ee7a5da61d78d9a5bcd
june <juneli@spartaglobal.com>
2022-05-13 15:02:17 +0100

    update readme with use instructions and description

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # Data 29 Star Wars Project
 
 
-## Aim
-This repo will return a collection of starships including the pilot name(s), if any.
+### Aim
+This repo will return a collection of starships from the Star Wars API, where pilot names will be stored as an array
+of object IDs, where the object ID is a reference the relevant Star Wars character from a Characters collection.
+
+### Before using this repo
+This repo makes a few assumptions:
+1. you have a mongodb database named "starwars"
+2. within this database you have a collection named "characters"
+3. within that collection you have the properties of all the people from the Star Wars API stored
+
+### Navigating this repo
+#### \__main\__.py
+Use the __main__.py file to run the program as it will contain only the relevant function/class calls. 
+
+#### app folder
+This folder contains all relevant code to run the function and class calls. This in itself is split into three files,
+one for each of the user stories (see task board for more info on user stories).
+
+#### tests folder
+The tests folder will contain unit test files used for the files in the app folder. Here the tests are again split
+into files for the relevant user story code it is testing. Use ```pytest -v``` in terminal to test.
 
 
-## Using this repo
-Use the __main__.py file to run the program as it will contain only the relevant function/class calls.
-
-The app folder will contain all application code required for the function/class calls.
-The tests folder will contain unit test files used for the files in the app folder.
-
-
-## Task board
+### Task board
 https://guttural-waste-40d.notion.site/Star-wars-project-task-56f034f986104eb68443b0721f55ba53
--- starwars/tests/test_module_us3.py
+++ starwars/tests/test_module_us3.py
@@ -5,30 +5,30 @@ import pymongo
 client = pymongo.MongoClient()
 db = client["starwars"]
 
+
 class UnitTests(unittest.TestCase):
 
     def setUp(self) -> None:
         self.list = us3.list_starship()
-        self.list2 = us3.list_character()
 
     def test_list_starship(self):
         actual = len(self.list)
         expected = 36
         self.assertEqual(actual, expected, "Expected 36 entries in list")
 
-    # def test_replace_names(self):
-    #     test_character = db["test_character"]
-    #     test_starship = db["test_starship"]
-    #     db.test_character.insert_one({"name": "test pilot"})
-    #     db.test_starship.insert_one({"name": "starship 1", "pilot": "test pilot"})
-    #
-    #     self.replace = us3.replace_names()
-    #
-    #     actual = db.test_starship.find_one({"name":"starship 1"}, {"pilot":1}).json()
-    #     expected = db.test_character.find_one({"name":"test pilot"}, {"_id":1}).json()
-    #
-    #     test_character.drop()
-    #     test_starship.drop()
-    #
-    #     self.assertEqual(actual, expected, "Object id matches with that of the ")
+    def test_replace_names(self):
+        test_character = db["test_character"]
+        test_starship = db["test_starship"]
+        db.test_character.insert_one({"name": "test pilot"})
+        db.test_starship.insert_one({"name": "starship 1", "pilot": "test pilot"})
+
+        self.replace = us3.replace_names()
+
+        actual = db.test_starship.find_one({"name":"starship 1"}, {"pilot":1})
+        expected = db.test_character.find_one({"name":"test pilot"}, {"_id":1})
+
+        test_character.drop()
+        test_starship.drop()
+
+        self.assertEqual(actual, expected, "Object id matches with that of the ")
 
