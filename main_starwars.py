4e263cbcd38d61205b9db03042799faf3d917f97
cex <cexhw@Louises-MBP.cust.communityfibre.co.uk>
2022-05-13 14:13:47 +0100

    Updated comments to my code

--- main_starwars.py
+++ main_starwars.py
@@ -1,46 +1,26 @@
-# import json
-# import requests
-
-# # FUNCTION TO PULL ALL AVAILIBLE STARSHIPS FROM API
-# starship_request = requests.get('https://www.swapi.tech/api/starships') # pull data from an API file
-# starship_json = starship_request.json()
-# characters_request = requests.get('https://www.swapi.tech/api/people')
-# characters_json = characters_request.json() #requesting a json object of the result
-
-# starship_str = json.dumps(starship_json) #converting the a json file
-# characters_str = json.dumps(characters_json)
-# #FUNCTION TO CHECK INTO THE URL AND EXTRACT ALL STARSHIPS TO EXTRACT ALL AVAILIBLE DATA
-# for package in starship_json['results']:
-#     print(requests.get(package["url"]).json()) 
-
-# print(characters_str) #extracted all characters from the data to have also the ID's
-
-
-# 1. Ensure that imports from python library are set:
 import requests
 import pymongo
 import json
 
-# 2.Access to database 
+#Access to database 
 client = pymongo.MongoClient()
 db = client['starwars']
 
 #Created a function to extract data from API link
-website = 'https://www.swapi.tech/api/starships'
+web_link = 'https://www.swapi.tech/api/starships'
 def data(url):
-    starship_url = requests.get(url)
-    starship_url = starship_url.json()
-    return starship_url
+    ship_url = requests.get(url)
+    ship_url = ship_url.json() #converted to a JSON file
+    return ship_url
+starship_page = [value['url'] for value in data(web_link)['results']] #Extracted the first page from my API link
 
-starship_page = [value['url'] for value in data(website)['results']]
-
-
+#Function to get all the remaining ships from the different pages
 def get_url_for_all_pages():
-    current_page = data(website)
-    while current_page['next'] != None:
-        current_page = requests.get(current_page['next'])
-        current_page = current_page.json()
-        for item in current_page['results']:
+    page_1 = data(web_link)
+    while page_1['next'] != None: # ensuring that the pages i extract give me pages that have data so i ensure that data is not eaual to none
+        page_1 = requests.get(page_1['next'])
+        page_1 = page_1.json() #coverted to a JSON file
+        for item in page_1['results']:
             starship_page.append(item['url'])
     return starship_page
 
