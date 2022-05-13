9b1b714d64df4f7b17d74c08356c4107917e4078
Durgesh Murugan <durgeshmurugan7962@gmail.com>
2022-05-13 16:40:15 +0100

    updated_python_with_pep8

--- starwars/app/starships_notepad.py
+++ starwars/app/starships_notepad.py
@@ -1,20 +1,20 @@
-import requesting_sw
-import pymongo
-import json
-import requests
+#import requesting_sw
+#import pymongo
+#import json
+#import requests
 
-client = pymongo.MongoClient()
-db = client["starwars"]
+#client = pymongo.MongoClient()
+#db = client["starwars"]
 
 #db.drop_collection("starships")
 
-starships_req = requests.get("https://www.swapi.tech/api/starships")
-characters_req = requests.get("https://www.swapi.tech/api/people")
+#starships_req = requests.get("https://www.swapi.tech/api/starships")
+#characters_req = requests.get("https://www.swapi.tech/api/people")
 
 
-starships_req_json = starships_req.json()
+#starships_req_json = starships_req.json()
 
-characters_req_json = characters_req.json()
+#characters_req_json = characters_req.json()
 
 
 #print(starships_req_json["results"])
@@ -29,15 +29,18 @@ characters_req_json = characters_req.json()
 #    print(requests.get(i['url']).json())
     #starships_urls.append(requests.get(i['url']).json())
 
-urls_list = []
+#urls_list = []
 
-def get_urls():
-    for i in starships_req_json["results"]:
-        urls = requests.get(i['url']).json()
-        print(urls["result"]['properties']['pilots'])
+#def get_urls():
+ #   for i in starships_req_json["results"]:
+  #      urls = requests.get(i['url']).json()
+   #     print(urls["result"]['properties']['pilots'])
 
-def get_people():
-    pass
+#def get_people():
+#    for i in characters_req_json['results']:
+#        print(requests.get(i['_id']).json())
+
+
 #{'message': 'ok', 'result': {'properties': {'model': 'CR90 corvette', 'starship_class': 'corvette', 'manufacturer': 'Corellian Engineering Corporation', 'cost_in_credits': '3500000', 'length': '150', 'crew': '30-165', 'passengers': '600', 'max_atmosphering_speed': '950', 'hyperdrive_rating': '2.0', 'MGLT': '60', 'cargo_capacity': '3000000', 'consumables': '1 year', 'pilots': [], 'created': '2020-09-17T17:55:06.604Z', 'edited': '2020-09-17T17:55:06.604Z', 'name': 'CR90 corvette', 'url': 'https://www.swapi.tech/api/starships/2'}, 'description': 'A Starship', '_id': '5f63a34fee9fd7000499be1e', 'uid': '2', '__v': 0}}
 
 
@@ -55,8 +58,8 @@ def get_people():
 #for i in characters_req_json['results']:
  #   print(requests.get(i['url']).json())
 
-for i in characters_req_json['results']:
-    print(requests.get(i['_id']).json())
+#for i in characters_req_json['results']:
+#    print(requests.get(i['_id']).json())
 
 
 # not urls, object ids of database
